8de48b024474aac7bb2e26b9154d7a67e630dcf0
Aluno SENAI LAURO <aluno@SENAI-LAURO.LOCAL>
2024-06-17 20:49:36 -0300

    Código principal- sistema_vendas.cpp

--- sistema_vendas.cpp
+++ sistema_vendas.cpp
@@ -13,14 +13,14 @@
 typedef struct {
     int id;
     char nome[MAX_CARACTER];
-} Cliente;
+    float preco;
+    int quantidade;
+} Produto;
 
 typedef struct {
     int id;
     char nome[MAX_CARACTER];
-    float preco;
-    int quantidade;
-} Produto;
+} Cliente;
 
 typedef struct {
     char nomecliente[MAX_CARACTER];
@@ -62,16 +62,21 @@ int tratamento(char analise[], int teste){
 	
 	return 0;
 }
-// Função para cadastrar cliente
+
+// Modulo venda - cadastrar cliente
 void cadastrarCliente() {
     char buffer[MAX_CARACTER];
 
     if (numClientes < MAX_CLIENTES) {
+    	do{
+    		printf("Digite o nome do cliente: ");
+        	scanf(" %49[^\n]s", buffer);
+        	
+		}while(0);
         printf("Digite o nome do cliente: ");
         scanf(" %49[^\n]s", buffer);
 
         strcpy(clientes[numClientes].nome, buffer);
-        clientes[numClientes].id = numClientes + 1;
         numClientes++;
 
         printf("Cliente cadastrado com sucesso!\n");
@@ -80,32 +85,62 @@ void cadastrarCliente() {
     }
 }
 
-// Função para adicionar quantidade ao estoque de um produto
-void adicionar(Produto produtos[], int numProduto) {
+// Modulo estoque - Adiciona uma quantidade ao estoque de um produto
+void adicionar(Produto produtos[], int numProdutos) {
     char nomeP[MAX_CARACTER];
     int qtdP;
+    int i;
+    int j;
 
     printf("Digite o nome do produto: ");
-    scanf(" %49[^\n]s", nomeP);
+    scanf("%s", nomeP);
 
-    printf("quantidade a ser adicionadas: ");
+    printf("Digite 1 para adicionar produto ou digite 2 para remover: ");
+    scanf("%d", &j);
+   
+switch (j) {
+        case 1:
+    printf("Digite a quantidade do produto a ser adicionada: ");
     scanf("%d", &qtdP);
-
-    if(qtdP<=0){
-        printf("a quantidade escolhida deve ser possitiva!\n");
+   
+    if (qtdP <= 0) {
+        printf("A quantidade escolhida deve ser positiva!\n");
+        return;
     }
 
-    for (int i = 0; i < MAX_PRODUTOS; i++) {
+    for (i = 0; i < numProdutos; i++) {
         if (strcmp(produtos[i].nome, nomeP) == 0) {
             produtos[i].quantidade += qtdP;
-
             printf("Quantidade de %s atualizada para %d.\n", produtos[i].nome, produtos[i].quantidade);
+            return;
         }
     }
-    printf("Produto de nome %s não encontrado.\n", nomeP);
+    printf("Produto de nome %s n?o encontrado.\n", nomeP);
+        break;
+   
+        case 2:
+        printf("Digite a quantidade do produto a ser removida: ");
+    scanf("%d", &qtdP);
+   
+    if (qtdP <= 0) {
+        printf("A quantidade escolhida deve ser positiva!\n");
+        return;
+    }
+
+    for (i = 0; i < numProdutos; i++) {
+        if (strcmp(produtos[i].nome, nomeP) == 0) {
+            produtos[i].quantidade -= qtdP;
+            printf("Quantidade de %s atualizada para %d.\n", produtos[i].nome, produtos[i].quantidade);
+            return;
+        }
+    }
+    printf("Produto de nome %s n?o encontrado.\n", nomeP);
+        break;
+       
+}
 }
 
-// Função para cadastrar produto
+// Modulo estoque - Cadastra produto
 void cadastrarProduto() {
     if (numProdutos < MAX_PRODUTOS) {
         Produto p;
@@ -124,7 +159,7 @@ void cadastrarProduto() {
     }
 }
 
-// Função para realizar uma compra
+// Modulo venda - realiza uma compra
 void comprar() {
     char buffer[MAX_CARACTER], compara[MAX_CARACTER];
     int qtd_compras, quantidade, i, x, z;
@@ -144,11 +179,9 @@ void comprar() {
 
     for (i = 0; i < numClientes; i++) {
         strcpy(compara, clientes[i].nome);
-
         for (x = 0; compara[x] != '\0'; x++) {
             compara[x] = tolower(compara[x]);
         }
-
         if (strcmp(compara, buffer) == 0) {
             achou = 1;
             break;
@@ -162,7 +195,6 @@ void comprar() {
 
     buffer[0] = toupper(buffer[0]);
     achou = 0;
-
     strcpy(carrinho.nomecliente, buffer);
 
     printf("Digite o número de produtos que deseja comprar: ");
@@ -175,7 +207,6 @@ void comprar() {
 
     for (i = 0; i < qtd_compras; i++) {
         achou = 0;
-
         printf("Digite o nome do %d produto: ", i + 1);
         scanf(" %49[^\n]s", buffer);
 
@@ -185,7 +216,6 @@ void comprar() {
 
         for (x = 0; x < numProdutos; x++) {
             strcpy(compara, produtos[x].nome);
-
             for (z = 0; compara[z] != '\0'; z++) {
                 compara[z] = tolower(compara[z]);
             }
@@ -194,14 +224,15 @@ void comprar() {
                 achou = 1;
 
                 if (produtos[x].quantidade == 0) {
-                    printf("Não há quantidade deste produto no estoque...\n");
+                    printf("Não quantidade deste produto no estoque...\n");
                     break;
                 }
 
                 do {
-                    printf("Quantidade de '%s' em estoque - %d;\n", produtos[x].nome, produtos[x].quantidade);
+                    printf("Quantidade de \'%s\' em estoque: %d;\n", produtos[x].nome, produtos[x].quantidade);
                     printf("Digite a quantidade do %d produto: ", i + 1);
                     scanf("%d", &quantidade);
+
                     system("cls");
                 } while (!(quantidade > 0 && quantidade <= produtos[x].quantidade));
 
@@ -227,7 +258,7 @@ void comprar() {
     carrinho.num_produtos = numCompras;
 }
 
-// Função para listar todos os clientes
+// Modulo venda - Lista todos os clientes
 void listarClientes() {
     int i;
     printf("Lista de Clientes: \n");
@@ -236,33 +267,86 @@ void listarClientes() {
     }
 }
 
-// Função para listar todos os produtos
+// Modulo estoque - Lista todos os produtos
 void listarProdutos() {
     int i;
     for (i = 0; i < numProdutos; i++) {
-        printf("ID: %d, Nome: %s, Preço: %.2f, Quantidade: %d\n", produtos[i].id, produtos[i].nome, produtos[i].preco, produtos[i].quantidade);
+        printf("ID: %d, Nome: %s, Preço: %.2f, quantidade: %d\n", produtos[i].id, produtos[i].nome, produtos[i].preco, produtos[i].quantidade);
     }
 }
 
-// Função para listar todas as compras efetuadas
+// Modulo vendas - calcula e retorna o total do carrinho
+float calcularTotalCarrinho() {
+	int i;
+    float total = 0.0;
+    for (i = 0; i < carrinho.num_produtos; i++) {
+        total += carrinho.preco[i] * carrinho.quantidade[i];
+    }
+    return total;
+}
+
+// Modulo vendas - lista todas as compras efetuadas
 void listarCompras() {
     int j;
     printf("Nome cliente: %s\n", carrinho.nomecliente);
-
     for (j = 0; j < carrinho.num_produtos; j++) {
-        printf("Nome: %s, Quantidade: %d, Preço: %.2f\n", carrinho.nomeproduto[j], carrinho.quantidade[j], carrinho.preco[j]);
+        printf("Produto: %s, Quantidade: %d, Preço: %.2f\n", carrinho.nomeproduto[j], carrinho.quantidade[j], carrinho.preco[j]);
     }
+    float total = calcularTotalCarrinho();
+    printf("Total do carrinho: %.2f\n", total);
 }
 
-// Função para excluir um produto do estoque
-void delete_product(Produto estoque[], int *num_produtos, int produto_id) {
+// Modulo vendas/estoque - Permite a troca de usuários
+int login() {
+	char buffer[MAX_CARACTER];
+    int senha_usuario;
+    int cod_usuario;
+	
+	do{
+		
+	    printf("Informe o login: ");
+	    scanf("%s", buffer);
+	    
+	    if(tratamento(buffer, 2)){
+	    	system("cls");
+	    	
+	    	printf("ERRO!! Contém letras...\n");
+	    	continue;
+		}
+		
+		sscanf(buffer, "%d", &cod_usuario);
+	    
+		printf("Informe a senha: ");
+	    scanf("%s", buffer);
+	    
+	    if(tratamento(buffer, 2)){
+	    	system("cls");
+	    	
+	    	printf("ERRO!! Contém letras...\n");
+	    	continue;
+		}
+		
+		sscanf(buffer, "%d", &senha_usuario);
+	    
+	    
+	}while(0);
+    // Vendas
+    if (cod_usuario == 2525 && senha_usuario == 1234) {
+        return 1;
+    }
+    
+    return 0;
+}
+
+// Modulo estoque - Função para excluir um produto do estoque pelo nome
+void delete_product(Produto produtos[], int *numProdutos, char nomeP[]) {
     bool produto_existe = false;
     int indice = -1;
-    int i;  // Declare a variável fora do loop
+    int i;
 
     // Verificar se o produto existe no estoque
-    for (i = 0; i < *num_produtos; i++) {  // Use a variável aqui
-        if (estoque[i].id == produto_id) {
+    for (i = 0; i < *numProdutos; i++) {
+        if (strcmp(produtos[i].nome, nomeP) == 0) {
             produto_existe = true;
             indice = i;
             break;
@@ -270,180 +354,95 @@ void delete_product(Produto estoque[], int *num_produtos, int produto_id) {
     }
 
     if (!produto_existe) {
-        printf("Produto com ID %d não encontrado no estoque.\n", produto_id);
+        printf("Produto de nome %s não encontrado no estoque.\n", nomeP);
         return;
     }
 
-    // Remover o produto do estoque e ajustar a lista
-    for (i = indice; i < *num_produtos - 1; i++) {  // Use a variável aqui
-        estoque[i] = estoque[i + 1];
+    // Remover o produto do estoque e ajustar a lista de produtos
+    for (i = indice; i < *numProdutos - 1; i++) {
+        produtos[i] = produtos[i + 1];
     }
-    (*num_produtos)--;
 
-    printf("Produto com ID %d foi removido do estoque.\n", produto_id);
-}
-
-// Função para trocar de usuário
-int trocarUsuario() {
-	char buffer[4];
-    int senha_usuario;
-    int cod_usuario;
-	
-	while(1){
-	    printf("Informe o login: ");
-	    scanf("%s", buffer);
-	    
-	    if(tratamento(buffer, 2)){
-	    	system("cls");
-	    	
-	    	printf("ERRO!! Foi digitado algo diferente de um número...\n");
-	    	printf("Por favor, tente novamente...\n");
-	    	
-	    	continue;
-		}
-		
-		sscanf(buffer, "%d", &cod_usuario);
-		
-	    printf("Informe a senha: ");
-	    scanf("%s", buffer);
-	    
-	    if(tratamento(buffer, 2)){
-	    	system("cls");
-	    	
-	    	printf("ERRO!! Foi digitado algo diferente de um número...\n");
-	    	printf("Por favor, tente novamente...\n");
-	    	
-	    	continue;
-		}
-	    
-	    sscanf(buffer, "%d", &senha_usuario);
-	
-	    // Vendas
-	    if (cod_usuario == 2525 && senha_usuario == 1234) {
-	        return 1;
-	    }
-	    // Estoque
-	    if (cod_usuario == 9973 && senha_usuario == 4321) {
-	        return 2;
-	    }
-	    // Senha ou login errados
-	    return -1;
-	}
+    (*numProdutos)--;
+    printf("Produto removido com sucesso!\n");
 }
 
 int main() {
-	char buffer[4];
-    int login, opcao = -1, produto_id;
+    int opcao = -1;
+    char nomeP[MAX_CARACTER], buffer[MAX_CARACTER];
 
     setlocale(LC_ALL, "Portuguese");
 
     do {
-        login = trocarUsuario();
-
-        if (login == 1) {
+        if (login()) {
             do {
                 printf("1. Cadastrar Cliente\n");
-                printf("2. Realizar Venda\n");
-                printf("3. Listar Compras\n");
-                printf("4. Trocar de Usuário\n");
-                printf("0. Sair\n");
+                printf("2. Cadastrar produto\n");
+                printf("3. Adicionar/remover produto\n");
+                printf("4. Deletar produto\n");
+                printf("5. Realizar Venda\n");
+                printf("6. Listar Carrinho\n");
+                printf("7. Listar clientes\n");
+                printf("8. Listar Produtos\n");
+                printf("9. Sair\n");
                 printf("Escolha uma opção: ");
                 scanf("%s", buffer);
                 
                 system("cls");
                 
                 if(tratamento(buffer, 2)){
-                	printf("ERRO!! Foi digitado algo que não era um número...\n");
-                	printf("Por favor tente novamente.\n");
+                	printf("ERRO!! Contém letras...\n");
                 	
-                	opcao = -1;
                 	continue;
 				}
-			
-				sscanf(buffer, "%d", &opcao);
 				
+				sscanf(buffer, "%d", &opcao);
+
                 switch (opcao) {
                     case 1:
                         cadastrarCliente();
                         break;
                     case 2:
-                        comprar();
-                        break;
+                    	cadastrarProduto();
+                    break;
                     case 3:
-                        listarCompras();
-                        break;
-                    case 4:
-                        login = -1;
-                        break;
-                    case 0:
-                        login = -1;
-                        break;
-                    default:
-                        printf("Opção inválida, por favor tente novamente...\n");
-                        break;
-                }
-			
-            } while (opcao != 0 && opcao != 4);
-        } else if (login == 2) {
-            do {
-                printf("1. Cadastrar Produto\n");
-                printf("2. Adicionar Produto\n");
-                printf("3. Listar Produtos\n");
-                printf("4. Trocar Usuário\n");
-                printf("5. Remover Produto\n");
-                printf("0. Sair\n");
-                printf("Escolha uma opção: ");
-                scanf("%s", buffer);
-
-                system("cls");
-                
-                if(tratamento(buffer, 2)){
-                	printf("ERRO!! Foi digitado algo que não era um número...\n");
-                	printf("Por favor, tente novamente...\n");
-                	
-                	opcao = -1;
-                	
-                	continue;
-				}
-				
-				sscanf(buffer, "%d", &opcao);
-
-                switch (opcao) {
-                    case 1:
-                        cadastrarProduto();
-                        break;
-                    case 2:
-                        adicionar(produtos, numProdutos);
-                        break;
-                    case 3:
-                        listarProdutos();
-                        break;
-                    case 4:
-                        login = -1;
-                        break;
-                    case 5: {
-                        printf("Digite o ID do produto a ser removido: ");
-                        scanf("%d", &produto_id);
+                    	adicionar(produtos, numProdutos);
+                    break;
+                    case 4:	
+                    	printf("Digite o nome do produto a ser removido: ");
+                        scanf(" %49[^\n]s", buffer);
                         
-                        delete_product(produtos, &numProdutos, produto_id);
-                        break;
-                    }
-                    case 0:
-                        login = -1;
-                        break;
+                        if(tratamento(buffer, 1)){
+                        	printf("ERRO!! Possui números...\n");
+                        }
+                        else{
+                        	sscanf(buffer, " %49[^\n]s", nomeP);
+                        	
+                        	delete_product(produtos, &numProdutos, nomeP);
+						}
+                    break;
+                    case 5:
+                    	comprar();
+                    break;
+                    case 6:
+                    	listarCompras();
+                    break;
+                    case 7:
+                    	listarClientes();
+                    break;
+                    case 8:
+                    	listarProdutos();
+                    case 9:
+                    break;
                     default:
                         printf("Opção inválida, por favor tente novamente...\n");
-                        break;
+                    break;
                 }
-            } while (opcao != 0 && opcao != 4);
-        } else {
-
-            system("cls");
-            printf("Usuário e/ou senha incorreta(as)...\n");
-        }
-    } while (opcao != 0);
+            }while(opcao != 9);
+    	}
+    } while (1);
     
-    printf("FIM\n");
     return 0;
 }
+
 
