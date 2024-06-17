aaf300daf58006ab21a25de8e1566bcd34a9960c
Marcos Conde <155701351+MarcosCondeJr@users.noreply.github.com>
2024-06-17 00:31:50 -0300

    paginas
    Feito o direcionais para as paginas, 
    
    paginas prontas:
    1 cadastrar vendedor
    2 cadastrar cliente
    3 listar carrinho
    5 cadastrar produto
    6

--- frontend.c
+++ frontend.c
@@ -20,6 +20,7 @@ int main() {
     init_pair(1, COLOR_WHITE, COLOR_BLACK);    // COR DO TEXTO BRANCO, COR DE FUNDO PRETO
     init_pair(2, COLOR_RED, COLOR_BLACK);     // Texto vermelho para erros
     init_pair(3, COLOR_BLACK, COLOR_WHITE);    // Texto preto, fundo branco (para campos de entrada)
+    init_pair(4, COLOR_GREEN, COLOR_BLACK);
 
     bkgd(COLOR_PAIR(1)); // Definir o fundo da janela principal como branco
     cbreak();
@@ -177,19 +178,20 @@ void pag_principal() {
     wattron(pagina_prin, A_BOLD | COLOR_PAIR(1));
     print_large_text(pagina_prin, 2, 0, "SISTEMA DE VENDAS");
 
-    // Lado esquerdo (vendas)
-    mvwprintw(pagina_prin, 4, 3, "1 - Cadastrar Cliente");
-    mvwprintw(pagina_prin, 5, 3, "2 - Realizar Venda");
-    mvwprintw(pagina_prin, 6, 3, "3 - Listar Carrinho");
+    // Lado esquerdo
+    mvwprintw(pagina_prin, 4, 3, "1 - Cadastrar Vendedor");
+    mvwprintw(pagina_prin, 5, 3, "2 - Cadastrar Cliente");
+    mvwprintw(pagina_prin, 6, 3, "3 - Realizar Venda");
+    mvwprintw(pagina_prin, 7, 3, "4 - Listar Carrinho");
 
-    // Lado direito (estoque)
-    mvwprintw(pagina_prin, 4, width - strlen("4 - Cadastrar vendedor") - 3, "4 - Cadastrar vendedor");
-    mvwprintw(pagina_prin, 5, width - strlen("5 - Atualizar Estoque") - 3, "5 - Atualizar Estoque");
-    mvwprintw(pagina_prin, 6, width - strlen("6 - Consultar Produtos") - 3, "6 - Consultar Produtos");
-    mvwprintw(pagina_prin, 7, width - strlen("7 - Excluir Produto") - 3, "7 - Excluir Produto");
+    // Lado direito
+    mvwprintw(pagina_prin, 4, width - strlen("5 - Cadastrar Produto") - 3, "5 - Cadastrar Produto");
+    mvwprintw(pagina_prin, 5, width - strlen("6 - Atualizar Estoque") - 3, "6 - Atualizar Estoque");
+    mvwprintw(pagina_prin, 6, width - strlen("7 - Consultar Produtos") - 3, "7 - Consultar Produtos");
+    mvwprintw(pagina_prin, 7, width - strlen("8 - Excluir Produto") - 3, "8 - Excluir Produto");
 
     // Rodapé
-    mvwprintw(pagina_prin, 9, (width - strlen("8 - Sair ")) / 2, "8 Sair ");
+    mvwprintw(pagina_prin, 9, (width - strlen("9 - Sair ")) / 2, "9 Sair ");
 
     //Entrada da opção desejada
     mvwprintw(pagina_prin,12, (width - strlen("Enter:")) / 2-4, "Enter:");
@@ -201,33 +203,37 @@ void pag_principal() {
     switch (opc_desejada) {
         case '1':
             wgetch(pagina_prin);
-            cadastrar_cliente();
+            cadastrar_vendedor();
             break;
         case '2':
             wgetch(pagina_prin);
-            realizar_venda();
+            cadastrar_cliente();
             break;
         case '3':
             wgetch(pagina_prin);
-            listar_carrinho();
+            realizar_venda();
             break;
         case '4':
             wgetch(pagina_prin);
-            cadastrar_vendedor();
+            listar_carrinho();
             break;
         case '5':
             wgetch(pagina_prin);
-            atualizar_estoque();
+            cadastrar_produto();
             break;
         case '6':
             wgetch(pagina_prin);
-            consultar_produto();
+            atualizar_estoque();
             break;
         case '7':
             wgetch(pagina_prin);
-            excluir_produto();
+            consultar_produto();
             break;
         case '8':
+            wgetch(pagina_prin);
+            excluir_produto();
+            break;
+        case '9':
             wgetch(pagina_prin);
             pag_login();
             break;
@@ -241,15 +247,48 @@ void pag_principal() {
 
     // Aguarda a entrada do usuário para fechar
     wgetch(pagina_prin);
-
     delwin(pagina_prin);
 }
 
-void cadastrar_cliente(){
+void cadastrar_vendedor(){
+    WINDOW* cad_vend;
+    int startx, starty, width, height;
+    char vend[50];
 
+    height = HEIGHT;
+    width = WIDTH;
+    starty = (LINES - height) / 2;
+    startx = (COLS - width) / 2;
 
+    cad_vend = newwin(height, width, starty, startx);
+    wbkgd(cad_vend, COLOR_PAIR(1)); // Fundo branco
+    draw_rounded_box(cad_vend, 0, 0, height, width);
+    refresh();
+
+    wattron(cad_vend, A_BOLD | COLOR_PAIR(1));
+    print_large_text(cad_vend, 2, 0, "CADASTRAR VENDEDOR");
+
+    //Entrada do nome do Vendedor
+    mvwprintw(cad_vend, 5, 10, "Nome do Vendedor: ");
+    echo();
+    wgetnstr(cad_vend, vend, sizeof(vend) - 1);
+    noecho();
+
+    // Mensagem de vendedor cadastrado
+    wattron(cad_vend, A_BOLD | COLOR_PAIR(4)); // COR VERDE NO NOME CLIENTE CADASTRADO
+    mvwprintw(cad_vend, 7, (width - strlen("Vendedor cadastrado!")) / 2, "Vendedor cadastrado!");
+    wattroff(cad_vend, A_BOLD | COLOR_PAIR(4));
+
+    wrefresh(cad_vend);
+
+    wgetch(cad_vend);
+    pag_principal();
+}
+
+void cadastrar_cliente() {
     WINDOW* cad_cli;
     int startx, starty, width, height;
+    char client[50];
 
     height = HEIGHT;
     width = WIDTH;
@@ -258,15 +297,29 @@ void cadastrar_cliente(){
 
     cad_cli = newwin(height, width, starty, startx);
     wbkgd(cad_cli, COLOR_PAIR(1)); // Fundo branco
-    draw_rounded_box(cad_cli, 0, 0, height, width);
+    draw_rounded_box(cad_cli, 0, 0, height, width); //Quadro no terminal
     refresh();
 
     wattron(cad_cli, A_BOLD | COLOR_PAIR(1));
     print_large_text(cad_cli, 2, 0, "CADASTRAR CLIENTE");
 
-    wgetch(cad_cli);
-    delwin(cad_cli);
+    //Entrada do nome do Cliente
+    mvwprintw(cad_cli, 5, 10, "Nome do Cliente: ");
+    echo();
+    wgetnstr(cad_cli, client, sizeof(client) - 1);
+    noecho();
+
+    // Mensagem de cliente cadastrado
+    wattron(cad_cli, A_BOLD | COLOR_PAIR(4)); // COR VERDE NO NOME CLIENTE CADASTRADO
+    mvwprintw(cad_cli, 7, (width - strlen("Cliente cadastrado!")) / 2, "Cliente cadastrado!");
+    wattroff(cad_cli, A_BOLD | COLOR_PAIR(4));
+
+    wrefresh(cad_cli);
+
+    wgetch(cad_cli); // Aguarda o usuário pressionar uma tecla para voltar para a pagina principal
+    pag_principal();
 }
+
 
 void realizar_venda(){
 
@@ -287,7 +340,7 @@ void realizar_venda(){
     print_large_text(rea_ven, 2, 0, "REALIZAR VENDA");
 
     wgetch(rea_ven);
-    delwin(rea_ven);
+    pag_principal();
 }
 
 void listar_carrinho(){
@@ -306,30 +359,56 @@ void listar_carrinho(){
 
     wattron(list_c, A_BOLD | COLOR_PAIR(1));
     print_large_text(list_c, 2, 0, "CARRINHO");
+    print_large_text(list_c,6,0,"CLIENTE: MARCOS CONDE");
+    print_large_text(list_c,8,0,"TOTAL DO CARRINHO: 0,0");
 
     wgetch(list_c);
-    delwin(list_c);
+    pag_principal();
 }
 
-void cadastrar_vendedor(){
-    WINDOW* cad_vend;
+void cadastrar_produto(){
+    WINDOW* cad_prod;
     int startx, starty, width, height;
+    char nome_pro[30],valor[20], qtd[20];
 
     height = HEIGHT;
     width = WIDTH;
     starty = (LINES - height) / 2;
     startx = (COLS - width) / 2;
 
-    cad_vend = newwin(height, width, starty, startx);
-    wbkgd(cad_vend, COLOR_PAIR(1)); // Fundo branco
-    draw_rounded_box(cad_vend, 0, 0, height, width);
+    cad_prod = newwin(height, width, starty, startx);
+    wbkgd(cad_prod, COLOR_PAIR(1)); // Fundo branco
+    draw_rounded_box(cad_prod, 0, 0, height, width);
     refresh();
 
-    wattron(cad_vend, A_BOLD | COLOR_PAIR(1));
-    print_large_text(cad_vend, 2, 0, "CADASTRAR VENDEDOR");
+    wattron(cad_prod, A_BOLD | COLOR_PAIR(1));
+    print_large_text(cad_prod, 2, 0, "CADASTRO DE PRODUTO");
 
-    wgetch(cad_vend);
-    delwin(cad_vend);
+    //Entrada do nome do produto
+    mvwprintw(cad_prod, 5, 10, "Nome do Produto: ");
+    echo();
+    wgetnstr(cad_prod, nome_pro, sizeof(nome_pro) - 1);
+    noecho();
+
+    //Entrada do valor do produto
+    mvwprintw(cad_prod, 6, 10, "Valor: ");
+    echo();
+    wgetnstr(cad_prod,valor, sizeof(valor)-1);
+    noecho();
+
+    //Entrada da quantidade do produto
+    mvwprintw(cad_prod, 7, 10, "Quantidade: ");
+    echo();
+    wgetnstr(cad_prod,qtd, sizeof(qtd)-1);
+    noecho();
+
+    // Mensagem de cliente cadastrado
+    wattron(cad_prod, A_BOLD | COLOR_PAIR(4)); // COR VERDE NO NOME CLIENTE CADASTRADO
+    mvwprintw(cad_prod, 10, (width - strlen("Produto cadastrado!")) / 2, "Produto cadastrado!");
+    wattroff(cad_prod, A_BOLD | COLOR_PAIR(4));
+
+    wgetch(cad_prod);
+    pag_principal();
 }
 
 void atualizar_estoque(){
@@ -350,7 +429,7 @@ void atualizar_estoque(){
     print_large_text(at_estoq, 2, 0, "ESTOQUE");
 
     wgetch(at_estoq);
-    delwin(at_estoq);
+    pag_principal();
 }
 
 void consultar_produto(){
@@ -371,7 +450,7 @@ void consultar_produto(){
     print_large_text(con_pro, 2, 0, "PRODUTOS");
 
     wgetch(con_pro);
-    delwin(con_pro);
+    pag_principal();
 }
 
 void excluir_produto(){
@@ -392,7 +471,7 @@ void excluir_produto(){
     print_large_text(excl, 2, 0, "EXCLUIR PRODUTO");
 
     wgetch(excl);
-    delwin(excl);
+    pag_principal();
 }
 
 
